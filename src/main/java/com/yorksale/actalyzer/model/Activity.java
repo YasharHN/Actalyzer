6158bb97349c85545f1c7fb9cb8f1596c2cead5b
Yashar <yhassanzadeh@bizinfogroup.ca>
2015-08-06 11:19:20 -0400

    Spark Service

--- src/main/java/com/yorksale/actalyzer/model/Activity.java
+++ src/main/java/com/yorksale/actalyzer/model/Activity.java
@@ -206,7 +206,6 @@ public class Activity implements Serializable {
 //        this.organizations = organizations;
 //    }
 
-
     public String getEmailAddress() {
         return emailAddress;
     }
